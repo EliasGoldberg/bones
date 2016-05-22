5bffe5301da6649d4b4cddbabfdde33c491ea21c
Jeremy Goldberg <elias.goldberg@gmail.com>
2016-05-22 11:11:08 -0400

    finished node.js install.

--- vagrant/scripts/node.sh
+++ vagrant/scripts/node.sh
@@ -1,11 +1,7 @@
-yum install -y git
-wget -q https://nodejs.org/dist/v4.4.4/node-v4.4.4.tar.gz
-mkdir node
-tar xf node-v*.tar.gz --strip-components=1 -C ./node
-rm -rf node-v*
-mkdir node/etc
-echo 'prefix=/usr/local' > node/etc/npmrc
-mv node /opt/
-chown -R root: /opt/node
-ln -s /opt/node/bin/node /usr/local/bin/node
-ln -s /opt/node/bin/npm /usr/local/bin/npm
+curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+yum install -y nodejs
+yum install -y gcc-c++ make
+
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
