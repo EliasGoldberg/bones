d4362c5f705ec6edb013f2c9451b0b75c797425a
Jeremy Goldberg <elias.goldberg@gmail.com>
2016-05-22 18:46:32 -0400

    now load balancing and monitoring two node.js processes
    setup pm2 to manage two node.js processes listening on different ports.
     setup nginx to load balancing between the two processes using a hash
    of the client’s entire ip address.

--- src/express/hello.js
+++ src/express/hello.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('This is Node JS\n');
-}).listen(8081);
+  res.end('This is Node JS on ' + process.env.PORT + '\n');
+}).listen(process.env.PORT,'127.0.0.1');
--- vagrant/scripts/node.sh
+++ vagrant/scripts/node.sh
@@ -1,3 +1,9 @@
 curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 yum install -y nodejs
 yum install -y gcc-c++ make
+
+yum install -y git
+npm install pm2 -g
+env PATH=$PATH:/usr/bin pm2 startup centos -u vagrant --hp /home/vagrant
+echo vagrant | su - vagrant -c "pm2 start /vagrant/files/pm2.json"
+echo vagrant | su - vagrant -c "pm2 save"
